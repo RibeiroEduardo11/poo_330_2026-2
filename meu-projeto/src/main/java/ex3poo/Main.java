fea7acc679ada4a39a838c549c271e4db9ed5910
Eduardo <dudumadke851@gmail.com>
2026-08-13 19:02:59 -0300

    aabaab

--- meu-projeto/src/main/java/ex3poo/Main.java
+++ meu-projeto/src/main/java/ex3poo/Main.java
@@ -1,4 +1,19 @@
 package ex3poo;
-
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] Args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o divisor");
+        int divisor = sc.nextInt();
+        System.out.println("Digite o dividendo");
+        int dividendo = sc.nextInt();
+        System.out.println("Digite a precisão");
+        int precisao = sc.nextInt();
+
+        Matematica m1 = new Matematica(dividendo,divisor,precisao);
+
+        System.out.println(m1.operacao(divisor,dividendo,precisao));
+    }
+
 }
--- meu-projeto/src/main/java/ex3poo/Matematica.java
+++ meu-projeto/src/main/java/ex3poo/Matematica.java
@@ -1,4 +1,48 @@
 package ex3poo;
 
 public class Matematica {
+    private int dividendo;
+    private int divisor;
+    private int precisao;
+
+    public Matematica(int dividendo, int divisor, int precisao){
+        this.divisor = divisor;
+        this.dividendo = dividendo;
+        this.precisao = precisao;
+    }
+
+    public int getDivisor(){
+        return divisor;
+    }
+    public int getDividendo(){
+        return dividendo;
+    }
+    public int getPrecisao(){
+        return precisao;
+    }
+    public void setDivisor(int divisor){
+        this.divisor = divisor;
+    }
+    public void setDividendo(int dividendo){
+        this.dividendo = dividendo;
+    }
+    public void setPrecisao(int precisao){
+        this.precisao = precisao;
+    }
+    public String toString(){
+        return "Divisor: "+divisor+
+                "\n Dividendo: "+dividendo+
+                "\n Precisão: "+precisao;
+    }
+    public static String operacao(int divisor, int dividendo, int precisao){
+        double operacao;
+        double aaa = divisor;
+        double bbb = dividendo;
+        operacao = aaa/bbb;
+        operacao = operacao*precisao;
+
+
+        return "O resultado é: "+operacao;
+    }
+
 }
